2dd13faf3dd40e8c523788402244267b0bf404d3
Ваше Имя <vampirleonardo@gmail.com>
2022-04-04 17:36:36 +0500

    added function ololo

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char const *argv[])
+{
+	
+		
+	int c = 0 ;
+	int b = 0;
+		scanf("%d",c+b);
+		printf("%d",c+b);
+	printf("%d\n", f);
+	scanf("%d", &c);
+	printf("Результат: %d\n", c);
+	printf("%d\n", argv[argc-1]);
+	
+	return 0;
+}   
+
+int sum(int a, int b)
+{
+
+app(a,b);	
+uint8_t c = 2;
+printf("%d\n", sum(a,b));
+	return (a + b);
+}
